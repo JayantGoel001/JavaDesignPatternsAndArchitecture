76269ba1e460d045aeea3c80712d053658de3704
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-30 16:36:52 +0530

    Initial Commit

--- src/Observer Pattern/view/ViewObserver.java
+++ src/Observer Pattern/view/ViewObserver.java
@@ -1,25 +1,59 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class ViewObserver extends JFrame {
-    private ModelObserver model;
+class ViewObserver extends JFrame implements ActionListener {
+    private final JButton helloButton;
 
     public ViewObserver(ModelObserver model) throws HeadlessException {
         super("MVC Demo");
-        this.model = model;
+
+        helloButton = new JButton("CLick Me");
+        JButton goodbyeButton = new JButton("CLick Me");
 
         setLayout(new GridBagLayout());
+
         GridBagConstraints gc = new GridBagConstraints();
         gc.anchor = GridBagConstraints.CENTER;
-        gc.gridheight = 0;
-        gc.gridwidth = 0;
+        gc.gridx = 1;
+        gc.gridy = 1;
         gc.weightx = 1;
         gc.weighty = 1;
         gc.fill = GridBagConstraints.NONE;
+
+        add(helloButton,gc);
+
+        gc.anchor = GridBagConstraints.CENTER;
+        gc.gridx = 1;
+        gc.gridy = 2;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+
+        add(goodbyeButton,gc);
+
+        helloButton.addActionListener(this);
+        goodbyeButton.addActionListener(this);
+
+        goodbyeButton.addActionListener(e -> {
+            System.out.println("Goodbye");
+        });
 
         setSize(600,500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton source = (JButton) e.getSource();
+        if (source == helloButton)
+        {
+            System.out.println("Hello World");
+        }
+        else {
+            System.out.println("Some Other Button.");
+        }
+    }
 }
