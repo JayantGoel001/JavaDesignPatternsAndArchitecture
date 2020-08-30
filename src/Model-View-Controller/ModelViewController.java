5e2849cc4ca0eb43af8371f88a48529471c6f6d5
username <54479676+CormacKrum@users.noreply.github.com>
2020-08-30 09:26:35 +0530

    Initial Commit

---
+++ src/Model-View-Controller/ModelViewController.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ModelViewController {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> {
+
+        });
+    }
+
+    public static void run(){
+
+    }
+}
