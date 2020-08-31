709cba22847c45dc3473cdee82554aeb3f59498b
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-01 05:14:17 +0530

    Initial Commit

---
+++ src/Model-View-Controller/model/PersonDAO.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class PersonDAO {
+    //CRUD = create,read,update and delete
+    public void addPerson(Person person){
+
+    }
+
+    public Person getPerson(int id){
+        return null;
+    }
+
+    public List<Person> getPeople(){
+        return null;
+    }
+
+    public void updatePerson(Person person){
+
+    }
+
+    public void deletePerson(int id){
+
+    }
+}
