3d46a4aa025042119eb51fcc40d586126d9334bb
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-02 04:20:01 +0530

    Initial Commit

--- Tests/PersonDAOTest.java
+++ Tests/PersonDAOTest.java
@@ -3,24 +3,29 @@ import org.junit.*;
 class PersonDAOTest {
     @BeforeClass
     public static void setUpBeforeClass() throws Exception{
-
+        Database.getInstance().connect();
     }
     @AfterClass
     public static void tearDownAfterClass() throws Exception{
-
+        Database.getInstance().disconnect();
     }
     @Before
     public void setUp() throws Exception{
-
+        System.out.println("Set Up");
     }
     @After
     public void tearDown() throws Exception{
-
+        System.out.println("Tear Down");
     }
     @Test
     public void testDemo(){
         int value = 2;
         value+=7;
         Assert.assertEquals("Check ",9,value);
+    }
+    @Test
+    public void testCreate(){
+        Person p1 = new Person("Bob","letmein");
+        Person p2 = new Person("Sue","Hello");
     }
 }
--- src/Model-View-Controller/model/Person.java
+++ src/Model-View-Controller/model/Person.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Person {
     private int id;
     private String name;
@@ -35,6 +37,15 @@ public class Person {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) &&
+                Objects.equals(password, person.password);
     }
 
     public void setPassword(String password) {
