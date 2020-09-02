74d1862dd86d38890705d1d237fecded8c91fa92
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-02 06:23:47 +0530

    Initial Commit

--- src/Adapter Pattern/AdapterPattern.java
+++ src/Adapter Pattern/AdapterPattern.java
@@ -1,7 +1,13 @@
 public class AdapterPattern {
     public static void main(String[] args) {
+        //Composition
         ConsoleLogWriter consoleWriter = new ConsoleLogWriter();
         Logger logger = new Logger(consoleWriter);
         logger.write("Hello");
+
+        //Inheritance
+        ConsoleLogWriter consoleLogWriter2 = new ConsoleLogWriter();
+        Logger logger2 = new Logger(consoleLogWriter2);
+        logger2.write("Hello2");
     }
 }
